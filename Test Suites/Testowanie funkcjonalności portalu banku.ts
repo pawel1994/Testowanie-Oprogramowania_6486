4948599ad3612ff8feb7657009a155856837f4a8
pawel1994 <58093877+pawel1994@users.noreply.github.com>
2021-01-16 13:19:35 +0100

    Add files via upload

---
+++ Test Suites/Testowanie funkcjonalności portalu banku.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Testowanie operacji wykonania szybkiego przelewu oraz doładowania telefonu</description>
+   <name>Testowanie funkcjonalności portalu banku</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>124d1e4d-a2d1-472a-afd4-0b72834158d4</testSuiteGuid>
+   <testCaseLink>
+      <guid>0cf30d94-e733-4175-b7b4-6acb3e608cf6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Scenariusz/Warunek testowy/Operacja wykonania szybkiego przelewu</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>60266955-cdf8-465c-bd1a-f5041c809756</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Scenariusz/Warunek testowy/Operacja wykonania przelewu na telefon</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
